3f2c24e2c490cb84c5e0b479650097cf7f035e3f
Magesh Kuppan <tkmagesh77@gmail.com>
2023-07-19 01:28:19 +0530

    fp - sort - assignment

---
+++ 01-JS/functional-programming/index.js
@@ -0,0 +1,37 @@
+var products = [
+    { id: 6, name: 'Pen', cost: 50, units: 20, category: 'stationary' },
+    { id: 9, name: 'Ten', cost: 70, units: 70, category: 'stationary' },
+    { id: 3, name: 'Len', cost: 60, units: 60, category: 'grocery' },
+    { id: 5, name: 'Zen', cost: 30, units: 30, category: 'grocery' },
+    { id: 1, name: 'Ken', cost: 20, units: 80, category: 'utencil' },
+    { id: 7, name: 'Mouse', cost: 100, units: 20, category: 'electronics' }
+];
+
+/* 
+Write the functions for the following tasks:
+    - sort
+    - filter
+    - groupBy
+*/
+
+console.log('Initial List')
+console.table(products);
+
+console.log('Sorting')
+console.log('Sort products by id')
+function sortProductsById(){
+    //bubble sort
+}
+console.table(products);
+
+console.log('Sort any list by any attribute')
+function sort(/*  */){
+
+}
+console.log('products by cost')
+sort(/*  */)
+console.table(products)
+
+console.log('products by units')
+sort(/*  */)
+console.table(products)
