d94b707fe065ca851ba381067606a72699ed902e
Magesh Kuppan <tkmagesh77@gmail.com>
2023-07-19 21:20:14 +0530

    groupby - inputs

--- 01-JS/functional-programming/index.js
+++ 01-JS/functional-programming/index.js
@@ -7,6 +7,39 @@ var products = [
     { id: 7, name: 'Mouse', cost: 100, units: 20, category: 'electronics' }
 ];
 
+//expected output of grouping
+//group products by category
+var productsByCategory = {
+    'stationary' : [
+        { id: 6, name: 'Pen', cost: 50, units: 20, category: 'stationary' },
+        { id: 9, name: 'Ten', cost: 70, units: 70, category: 'stationary' }
+    ],
+    'grocery' : [
+        { id: 3, name: 'Len', cost: 60, units: 60, category: 'grocery' },
+        { id: 5, name: 'Zen', cost: 30, units: 30, category: 'grocery' }
+    ],
+    'utencil' : [
+        { id: 1, name: 'Ken', cost: 20, units: 80, category: 'utencil' }
+    ],
+    'electronics' : [
+        { id: 7, name: 'Mouse', cost: 100, units: 20, category: 'electronics' }
+    ]
+}
+
+//group products by cost
+var productsByCost = {
+    'costly' : [
+        { id: 9, name: 'Ten', cost: 70, units: 70, category: 'stationary' },
+        { id: 3, name: 'Len', cost: 60, units: 60, category: 'grocery' },
+        { id: 7, name: 'Mouse', cost: 100, units: 20, category: 'electronics' }
+    ],
+    'affordable' : [
+        { id: 6, name: 'Pen', cost: 50, units: 20, category: 'stationary' },
+        { id: 5, name: 'Zen', cost: 30, units: 30, category: 'grocery' },
+        { id: 1, name: 'Ken', cost: 20, units: 80, category: 'utencil' }
+    ]
+}
+
 function useCase(title, fn){
     console.group(title)
     fn()
@@ -232,5 +265,4 @@ useCase('Filter', function(){
             })
         })
     })
-
 })
