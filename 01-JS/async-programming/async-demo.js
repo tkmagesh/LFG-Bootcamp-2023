21515b2154feb320b5ea9be2b0f187aef758b978
Magesh Kuppan <tkmagesh77@gmail.com>
2023-07-20 21:17:49 +0530

    more on promises

--- 01-JS/async-programming/async-demo.js
+++ 01-JS/async-programming/async-demo.js
@@ -46,6 +46,60 @@
         return p;
     }
 
-    window['addAsyncPromise'] = addAsyncPromise
+    function addAsyncPromiseClient(){
+        console.log('[@client] invoking the service');
+        var p = addAsyncPromise(100,200);
+        p.then(function(result){
+            console.log('[@client] result = ', result)
+        })
+    }
+
+    window['addAsyncPromiseClient'] = addAsyncPromiseClient
+
+    function divideAsyncPromise(x,y){
+        console.log('   [@service] processing', x , ' and ', y)
+        var p = new Promise(function(resolveFn, rejectFn){
+            setTimeout(function(){
+                if (y === 0){
+                    var err = new Error("divisor cannot be zero")
+                    rejectFn(err)
+                    return
+                }
+                var result = x / y;
+                console.log('   [@service] returing the result')
+                //communicate the result to the promise?
+                resolveFn(result)
+            }, 5000)
+        })
+        return p;
+    }
+
+    /* 
+    function divideAsyncPromiseClient(x,y){
+        console.log('[@client] invoking the service');
+        var p = divideAsyncPromise(x,y);
+        p.then(function(result){
+            console.log('[@client] result = ', result)
+        })
+        p.catch(function(err){
+            console.log('promise rejected with error :', err)
+        })
+    } 
+    */
+    function divideAsyncPromiseClient(x,y){
+        console.log('[@client] invoking the service');
+        divideAsyncPromise(x,y)
+            .then(function(result){
+                console.log('[@client] result = ', result)
+            })
+            .catch(function(err){
+                console.log('promise rejected with error :', err)
+            })
+            .finally(function(){
+                console.log('async operation completed')
+            })
+    }
+
+    window['divideAsyncPromiseClient'] = divideAsyncPromiseClient
     
 })()
