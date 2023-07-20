245947895d1615993d706afaaf942fccc632a2aa
Magesh Kuppan <tkmagesh77@gmail.com>
2023-07-20 19:33:57 +0530

    updates

--- 01-JS/async-programming/async-demo.js
+++ 01-JS/async-programming/async-demo.js
@@ -13,5 +13,22 @@
     }
 
     window['addSyncClient'] = addSyncClient;
-        
+
+    function addAsync(x,y){
+        console.log('   [@service] processing', x , ' and ', y)
+        setTimeout(function(){
+            var result = x + y;
+            console.log('   [@service] returing the result')
+            return result;
+        }, 5000)
+    }
+
+    function addAsyncClient(){
+        console.log('[@client] invoking the service')
+        var result = addAsync(100,200)
+        console.log('[@client] result = ', result)
+    }
+
+    window['addAsyncClient'] = addAsyncClient;
+
 })()
