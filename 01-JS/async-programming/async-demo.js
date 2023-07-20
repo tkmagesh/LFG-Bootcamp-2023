fc6b9511696c29b6ddd2f313bcadee9e4b13b7dc
Magesh Kuppan <tkmagesh77@gmail.com>
2023-07-20 21:00:35 +0530

    Promises - intro

--- 01-JS/async-programming/async-demo.js
+++ 01-JS/async-programming/async-demo.js
@@ -14,7 +14,7 @@
 
     window['addSyncClient'] = addSyncClient;
 
-    function addAsync(x,y, callbackFn /* phone number */){
+    function addAsyncCallback(x,y, callbackFn /* phone number */){
         console.log('   [@service] processing', x , ' and ', y)
         setTimeout(function(){
             var result = x + y;
@@ -24,13 +24,28 @@
         return
     }
 
-    function addAsyncClient(){
+    function addAsyncCallbackClient(){
         console.log('[@client] invoking the service')
         addAsync(100,200, function(result){
             console.log('[@client] result = ', result)
         })
     }
 
-    window['addAsyncClient'] = addAsyncClient;
+    window['addAsyncCallbackClient'] = addAsyncCallbackClient;
 
+    function addAsyncPromise(x,y){
+        console.log('   [@service] processing', x , ' and ', y)
+        var p = new Promise(function(resolveFn, rejectFn){
+            setTimeout(function(){
+                var result = x + y;
+                console.log('   [@service] returing the result')
+                //communicate the result to the promise?
+                resolveFn(result)
+            }, 5000)
+        })
+        return p;
+    }
+
+    window['addAsyncPromise'] = addAsyncPromise
+    
 })()
