d419289e63243d5a897de0a1328218aa0f8e624a
Magesh Kuppan <tkmagesh77@gmail.com>
2023-07-21 21:57:41 +0530

    Day-05 complete

--- 02-TypeScript/ts-app/dist/index.js
+++ 02-TypeScript/ts-app/dist/index.js
@@ -3,12 +3,14 @@ let x = 100;
 //type inference
 let y = 200; // => let y : number = 200
 //any
-let z; // defaults to 'any' type => let z : any;
+// let z : any; // defaults to 'any' type => let z : any;
+let z;
 z = 100;
 z = "This is a string";
 z = true;
 z = [];
 z = 200;
+z = {};
 z = "This is a string";
 z = 100;
 console.log(z * 2);
@@ -24,3 +26,86 @@ uk_var = "This is a string";
 if (typeof uk_var === 'string') {
     console.log(uk_var.length);
 }
+// function arguments & return values
+function multiply(x, y) {
+    return x * y;
+    // return "dummy string"
+}
+function doSomething(z) {
+    console.log(z);
+    // return 100;
+}
+function count(data) {
+    return data.length;
+}
+console.log(count("Cillum cupidatat veniam tempor consequat mollit dolor non proident. Officia irure ea esse magna eu sint. Occaecat quis cillum laboris consectetur voluptate commodo."));
+console.log(count([10, 20, 30, 40]));
+let p = {
+    id: 100,
+    name: 'Pen',
+    cost: 10,
+};
+let e1 = {
+    id: 100,
+    name: "Magesh",
+    city: "Bangalore"
+};
+let e2 = {
+    id: 200,
+    name: 'John'
+};
+// class
+class Employee {
+    //accessor methods
+    get id() {
+        return this._id;
+    }
+    set id(value) {
+        if (value < 0)
+            throw new Error('id cannot be negative');
+        this._id = value;
+    }
+    //constructor method
+    constructor(id, name, city) {
+        //instance attributes
+        //private attribute
+        this._id = 0;
+        //protected => accessible in the Employee and its derived classes but not through object instances
+        this.deptCode = 'DP-DEV';
+        //public attributes
+        this.name = '';
+        //readonly attribute
+        this.city = '';
+        this.id = id;
+        this.name = name;
+        if (city) {
+            this.city = city;
+        }
+    }
+    //intance methods
+    format() {
+        return `id = ${this.id}, name = ${this.name}, city = ${this.city}`;
+    }
+    //static method
+    static IsEmployee(obj) {
+        return obj instanceof Employee;
+    }
+}
+//static attribute
+Employee.version = '1.0';
+let emp = new Employee(100, 'Magesh', 'Bangalore');
+class FullTimeEmployee extends Employee {
+    constructor(id, name, city, benefits) {
+        super(id, name, city);
+        this.benefits = '';
+        this.benefits = benefits;
+    }
+    format() {
+        return `${super.format()}, benefits = ${this.benefits}, deptCode = ${this.deptCode}`;
+    }
+}
+let fte = new FullTimeEmployee(200, 'Philip', 'New Delhi', 'Travel Allowance');
+console.log(fte.format());
+// console.log(fte.deptCode)
+// arrow functions
+let add = (x, y) => x + y;
--- 02-TypeScript/ts-app/index.ts
+++ 02-TypeScript/ts-app/index.ts
@@ -4,12 +4,14 @@ let x : number = 100;
 let y = 200; // => let y : number = 200
 
 //any
-let z : any; // defaults to 'any' type => let z : any;
+// let z : any; // defaults to 'any' type => let z : any;
+let z;
 z = 100
 z = "This is a string"
 z = true
 z = []
 z = 200
+z = {}
 
 z = "This is a string"
 z = 100
@@ -29,3 +31,133 @@ uk_var = "This is a string"
 if (typeof uk_var === 'string'){
     console.log(uk_var.length)
 }
+
+// function arguments & return values
+function multiply(x : number, y : number) : number {
+    return x * y;
+    // return "dummy string"
+}
+
+function doSomething(z : any) : void {
+    console.log(z)
+    // return 100;
+}
+
+
+// Defining types
+type Countable = string | any[];
+
+function count(data : Countable ) {
+    return data.length;
+}
+console.log(count("Cillum cupidatat veniam tempor consequat mollit dolor non proident. Officia irure ea esse magna eu sint. Occaecat quis cillum laboris consectetur voluptate commodo."))
+console.log(count([10,20,30,40]))
+
+// Object type
+/* 
+let p : { id : number, name : string, cost : number } = {
+    id : 100,
+    name : 'Pen',
+    cost : 10,
+    category : 'Stationary'
+} 
+*/
+type Product = { 
+    id : number, 
+    name : string, 
+    cost : number 
+}
+let p : Product = {
+    id : 100,
+    name : 'Pen',
+    cost : 10,
+}
+
+//optional attributes
+type EmployeeType = {
+    id : number,
+    name : string,
+    city? : string
+}
+let e1 : EmployeeType = {
+    id : 100,
+    name : "Magesh",
+    city : "Bangalore"
+}
+
+let e2 : EmployeeType = {
+    id : 200,
+    name : 'John'
+}
+
+// class
+class Employee {
+    //instance attributes
+    //private attribute
+    private _id = 0;
+
+    //protected => accessible in the Employee and its derived classes but not through object instances
+    protected deptCode : string = 'DP-DEV'
+
+    //accessor methods
+    get id(){
+        return this._id;
+    }
+    set id(value){
+        if (value < 0) throw new Error('id cannot be negative')
+        this._id = value
+    }
+
+    //public attributes
+    name = '';
+
+    //readonly attribute
+    readonly city : string = '';
+
+    //constructor method
+    constructor(id : number, name : string, city? : string){
+        this.id = id;
+        this.name = name;
+        if (city){
+            this.city = city;
+        }
+
+    }
+
+    //intance methods
+    format(){
+        return `id = ${this.id}, name = ${this.name}, city = ${this.city}`
+    }
+
+    //static attribute
+    static version = '1.0'
+
+    //static method
+    static IsEmployee(obj : any) : boolean{
+        return obj instanceof Employee;
+    }
+}
+
+let emp = new Employee(100, 'Magesh', 'Bangalore')
+
+
+class FullTimeEmployee extends Employee {
+
+    benefits = '';
+
+    constructor(id : number, name : string, city : string, benefits : string){
+        super(id, name, city);
+        this.benefits = benefits;
+    }
+
+    format(){
+        return `${super.format()}, benefits = ${this.benefits}, deptCode = ${this.deptCode}`
+    }
+}
+
+let fte = new FullTimeEmployee(200, 'Philip', 'New Delhi', 'Travel Allowance')
+console.log(fte.format())
+
+// console.log(fte.deptCode)
+// arrow functions
+let add = (x : number, y : number) : number =>  x + y ;
