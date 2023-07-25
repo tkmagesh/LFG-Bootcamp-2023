bdf9f12f515e73be79cb8667fa2e3c911f79ec5f
Magesh Kuppan <tkmagesh77@gmail.com>
2023-07-25 19:37:43 +0530

    export & import variations

--- 02-TypeScript/ts-app-module/dist/index.js
+++ 02-TypeScript/ts-app-module/dist/index.js
@@ -1,2 +1,31 @@
-import * as utilsModule from './utils.js';
-console.log(utilsModule);
+// step-1
+/*
+import * as utilsModule from './utils.js'
+
+// console.log(utilsModule)
+console.log(utilsModule.add(100,200))
+console.log(utilsModule.subtract(100,200))
+*/
+// step-2 (simplifying step-1)
+/*
+import * as utilsModule from './utils.js'
+
+const add = utilsModule.add,
+    subtract = utilsModule.subtract
+
+console.log(add(100,200))
+console.log(subtract(100,200))
+*/
+// step-3
+/*
+import * as utilsModule from './utils.js'
+
+const { add, subtract } = utilsModule;
+
+console.log(add(100,200))
+console.log(subtract(100,200))
+*/
+//step-4
+import { add, subtract } from './utils.js';
+console.log(add(100, 200));
+console.log(subtract(100, 200));
