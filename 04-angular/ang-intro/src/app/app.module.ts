0e0cd2df99cf49164d40a4c0ece3093fa099c74e
Magesh Kuppan <tkmagesh77@gmail.com>
2023-07-26 02:29:43 +0530

    Day-07 complete

--- 04-angular/ang-intro/src/app/app.module.ts
+++ 04-angular/ang-intro/src/app/app.module.ts
@@ -4,13 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 @NgModule({
+  /* All the UI entities (component, directive, pipe) */
   declarations: [
     AppComponent
   ],
+  /* All the dependency modules  */
   imports: [
     BrowserModule
   ],
+  /* All the NON-UI entities (services) */
   providers: [],
+
+  /* top level components */
   bootstrap: [AppComponent]
 })
 export class AppModule { }
