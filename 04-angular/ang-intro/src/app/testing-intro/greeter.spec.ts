3788afe364fe742da95d0c9b75f977a75befc822
Magesh Kuppan <tkmagesh77@gmail.com>
2023-07-27 20:11:52 +0530

    Dependency Inversion Principle & Unit Testing

--- 04-angular/ang-intro/src/app/testing-intro/greeter.spec.ts
+++ 04-angular/ang-intro/src/app/testing-intro/greeter.spec.ts
@@ -1,12 +1,38 @@
-import { Greeter } from "./greeter"
+import { Greeter, IDateService } from "./greeter"
 
+
+class FakeDateServiceForMorning implements IDateService{
+    getCurrent(): Date {
+        return new Date(2023, 6, 27, 9, 0, 0)
+    }
+}
+
+class FakeDateServiceForEvening implements IDateService{
+    getCurrent(): Date {
+        return new Date(2023, 6, 27, 15, 0, 0)
+    }
+}
 
 describe('greeter', () => {
-    it('should greet the given user', () => {
+    it('should greet the given user with Good Morning when greeted before 12', () => {
         //arrange
-        const greeter = new Greeter();
+        const morningDateService = new FakeDateServiceForMorning()
+        const greeter = new Greeter(morningDateService);
         const userName = 'Magesh';
-        const expectedResult = 'Hi Magesh';
+        const expectedResult = 'Hi Magesh, Good Morning!';
+
+        //act
+        const actualResult = greeter.greet(userName);
+
+        //assert
+        expect(actualResult).toBe(expectedResult);
+    })
+    it('should greet the given user with Good Day when greeted after 12', () => {
+        //arrange
+        const eveningDateService = new FakeDateServiceForEvening()
+        const greeter = new Greeter(eveningDateService);
+        const userName = 'Magesh';
+        const expectedResult = 'Hi Magesh, Good Day!';
 
         //act
         const actualResult = greeter.greet(userName);
--- 04-angular/ang-intro/src/app/testing-intro/greeter.ts
+++ 04-angular/ang-intro/src/app/testing-intro/greeter.ts
@@ -1,6 +1,35 @@
 
-export class Greeter{
-    greet(userName : string) : string {
-        return `Hi ${userName}`
+export interface IDateService {
+    getCurrent() : Date
+}
+
+class DateService {
+    getCurrent() {
+        return new Date()
     }
 }
+
+export class Greeter{
+    _dateService : IDateService;
+
+    constructor(dateService : IDateService){
+        this._dateService = dateService;
+    }
+
+    greet(userName : string) : string {
+        // const currentHour = new Date().getHours()
+        const currentHour = this._dateService.getCurrent().getHours()
+        if (currentHour < 12 ) {
+            return `Hi ${userName}, Good Morning!`
+        } 
+        return `Hi ${userName}, Good Day!`
+    }
+}
+
+// modify the above class as follows:
+// when the greet() is invoked during morning ( < 12PM )
+    // greet with 'Hi <username>, Good Morning!'
+// else
+    // greet with 'Hi <username>, Good Day!'
+
+// Write the tests to test these changes
