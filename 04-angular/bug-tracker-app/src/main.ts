0d95d1631f28d9cb1dc420da5154359eb4d381a7
Magesh Kuppan <tkmagesh77@gmail.com>
2023-08-03 23:51:56 +0530

    using moment

--- 04-angular/bug-tracker-app/src/main.ts
+++ 04-angular/bug-tracker-app/src/main.ts
@@ -2,6 +2,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
 
+import * as moment from 'moment';
+console.log(moment('2023-08-03T13:58:21.220Z').fromNow());
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
