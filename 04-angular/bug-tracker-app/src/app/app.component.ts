ddeced2ca9ab881f5f4a09c6d8aa3c7aa6325792
Magesh Kuppan <tkmagesh77@gmail.com>
2023-08-04 22:13:10 +0530

    updates

--- 04-angular/bug-tracker-app/src/app/app.component.ts
+++ 04-angular/bug-tracker-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bug-tracker-app';
+
+  constructor(){
+    const obs$ = new Observable((subscriber => {
+      let count = 0;
+      setInterval(() => {
+        ++count
+        subscriber.next(count)
+      }, 1000)
+    }))
+
+    const subscriber = obs$.subscribe(val => console.log(val))
+
+    setTimeout(() => {
+      subscriber.unsubscribe()
+    }, 20000);
+  }
 }
