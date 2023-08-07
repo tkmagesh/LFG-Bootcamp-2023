81dd396fa7c3a7f4be0212cb6b35e68cd2baa958
Magesh Kuppan <tkmagesh77@gmail.com>
2023-08-07 19:40:54 +0530

    fromArray - assignment

---
+++ 05-rxjs/04-fromArray.js
@@ -0,0 +1,12 @@
+
+/* create a function that creates an observable to emit the data from the given array */
+
+let nos = [10,20,30,40,40,50]
+
+function fromArray(?){
+
+}
+
+let obs$ = fromArray(nos)
+
+// subscribe to the obs$ and print the data generated
