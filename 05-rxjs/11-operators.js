086924eb653ec7f6470976291cced871e6e80384
Magesh Kuppan <tkmagesh77@gmail.com>
2023-08-07 21:42:39 +0530

    operators intro

--- 05-rxjs/11-operators.js
+++ 05-rxjs/11-operators.js
@@ -1,14 +1,20 @@
-import { filter, map } from "rxjs/operators";
-import { interval } from "rxjs";
+import { debounceTime, filter, map, tap, delay, take, skip, takeUntil  } from "rxjs/operators";
+import { interval, timer } from "rxjs";
 
-let evenDoubleObs$ = interval(500)
+let stop$ = timer(2000);
+
+let evenDoubleObs$ = interval(100)
     .pipe(
         filter(no => no % 2 === 0),
-        map(no => no * 2)
+        tap(even => console.log(`[even no] = ${even}`)),
+        map(no => no * 2),
+        skip(5),
+        // take(10)
+        takeUntil(stop$)
     );
-    
+ 
 let evenDoubleObsSubscription = evenDoubleObs$.subscribe(evenDoubleNo => console.log(evenDoubleNo));
-
+/*
 setTimeout(() => {
     evenDoubleObsSubscription.unsubscribe()
-}, 5000); 
+}, 5000);  */
