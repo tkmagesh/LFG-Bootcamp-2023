7a8794a5d6c04ee63d7b850602fb19268e81a435
Magesh Kuppan <tkmagesh77@gmail.com>
2023-08-11 20:53:22 +0530

    component UI testing

--- 04-angular/ang-testing/src/app/components/greeter/greeter.component.spec.ts
+++ 04-angular/ang-testing/src/app/components/greeter/greeter.component.spec.ts
@@ -1,5 +1,6 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing"
 import { GreeterComponent } from "./greeter.component"
+import { By } from "@angular/platform-browser";
 
 fdescribe("Greeter Component (Reference ONLY)", () => {
 
@@ -29,10 +30,88 @@ fdescribe("Greeter Component (Reference ONLY)", () => {
             let greeterComponent = greeterFixture.componentInstance;
 
             greeterComponent.onBtnGreetClick("Magesh");
-            
+
             expect(greeterComponent.message).toBe('Hi Magesh! Good Morning!!')
         })
     });
 
+    describe("Component Presentation", () => {
+        it("should display the message from the component instance", () => {
+
+            //render the component
+            greeterFixture.detectChanges();
+
+            //query the elements for assertion
+            let divDebugElement = greeterFixture.debugElement.query(By.css("#divMessage"))
+            let divHTMLElement = divDebugElement.nativeElement
+            expect(divHTMLElement.textContent).toBe("A dummy message")
+        })
+
+        it("should display the message from the component instance when the message is changing", () => {
+
+            
+            // greeterFixture.componentInstance.message = 'test message'
+
+            let greeterComponentInstance = greeterFixture.componentInstance;
+            greeterComponentInstance.onBtnGreetClick('Magesh')
+
+            //render the component
+            greeterFixture.detectChanges();
+
+            //query the elements for assertion
+            let divDebugElement = greeterFixture.debugElement.query(By.css("#divMessage"))
+            let divHTMLElement = divDebugElement.nativeElement
+            expect(divHTMLElement.textContent).toBe('Hi Magesh! Good Morning!!')
+        })
+
+        it("should invoke onBtnGreetclick method when greet button is clicked", () => {
+
+            //creating a spy on 'onBtnGreetClick' to check if the method is invoked when the button is clicked
+            spyOn(greeterFixture.componentInstance, "onBtnGreetClick")
+        
+            // get the debugElement wrappter for the text box
+            let userNameTxtDebugElement = greeterFixture.debugElement.query(By.css('input[type="text"]'))
+
+            // set the value in the text box
+            userNameTxtDebugElement.nativeElement.value = "Magesh";
+
+            // get the debugElement wrapper for the greet button
+            let greetBtnDebugElement = greeterFixture.debugElement.query(By.css('#btnGreet'));
+
+            // trigger the click event of the button
+            greetBtnDebugElement.triggerEventHandler('click')
+
+            //render the component
+            greeterFixture.detectChanges();
+
+            //assert if the onBtnGreetClick() method is invoked when the button is clicked
+            expect(greeterFixture.componentInstance.onBtnGreetClick).toHaveBeenCalledWith('Magesh');
+
+            
+        })
+
+        it("should display the greet message when greet button is clicked", () => {
+
+            // get the debugElement wrappter for the text box
+            let userNameTxtDebugElement = greeterFixture.debugElement.query(By.css('input[type="text"]'))
+
+            // set the value in the text box
+            userNameTxtDebugElement.nativeElement.value = "Magesh";
+
+            // get the debugElement wrapper for the greet button
+            let greetBtnDebugElement = greeterFixture.debugElement.query(By.css('#btnGreet'));
+
+            // trigger the click event of the button
+            greetBtnDebugElement.triggerEventHandler('click')
+
+            //render the component
+            greeterFixture.detectChanges();
+
+            //query the elements for assertion
+            let divDebugElement = greeterFixture.debugElement.query(By.css("#divMessage"))
+            let divHTMLElement = divDebugElement.nativeElement
+            expect(divHTMLElement.textContent).toBe('Hi Magesh! Good Morning!!')
+        })
+    })
 
 }) 
