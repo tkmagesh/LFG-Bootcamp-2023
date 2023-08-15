b4a30e7c56249394df7b7f2ee6e169b0fa1c978e
Magesh Kuppan <tkmagesh77@gmail.com>
2023-08-16 01:02:52 +0530

    Creating, raising & handling exceptions

---
+++ 08-Python/14-exceptions.py
@@ -0,0 +1,43 @@
+""" 
+try:
+    do something
+except:
+    handle the error
+else:
+    do what needs to be done if there is no error
+finally:
+    any logic that has to be executed irrespective of success or exception
+    
+"""
+
+
+
+# function that raises a possible ZeroDivisionError
+def divide(x,y):
+    return x / y
+
+# custom exception
+class MyCustomError(Exception):
+    pass
+
+# function that raises our custom exception (MyCustomError)
+def do_something():
+    #raising (throwing) the error/exception
+    raise MyCustomError()
+
+# divisor = 0 # to raise the ZeroDivisionError
+divisor = 7
+try:
+    result = divide(100,divisor)
+    # print("abc" / 5)
+    do_something()
+except ZeroDivisionError:
+    print("do not attempt to divide by 0")
+except MyCustomError:
+    print("our custom exception was raised")
+except:
+    print("something went wrong")
+else:
+    print(f"result = {result}")
+finally:
+    print("divide operation completed")
