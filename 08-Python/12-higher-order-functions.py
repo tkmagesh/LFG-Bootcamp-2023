ca37ddca745d1a446db0a03d3562d7aea9ba0e61
Magesh Kuppan <tkmagesh77@gmail.com>
2023-08-15 23:44:02 +0530

    more lambda

--- 08-Python/12-higher-order-functions.py
+++ 08-Python/12-higher-order-functions.py
@@ -29,11 +29,15 @@ print(functools.reduce(lambda x,y: x + y, nos_list, 0))
 
 # Find the number of odd & even numbers from nos_list using reduce
 def count_odd_even(result, no):
+    """ 
     if no % 2 == 0:
         result['even'] += 1
     else:
         result['odd'] += 1
-    return result
+    return result 
+    """
+    return {'even' : result['even'] + 1, 'odd' : result['odd']} if no % 2 == 0 else { 'odd' : result['odd'] + 1, 'even' : result['even'] }
 
-nos_stats = functools.reduce(count_odd_even, nos_list, {'even' : 0, 'odd' : 0})
+# nos_stats = functools.reduce(count_odd_even, nos_list, {'even' : 0, 'odd' : 0})
+nos_stats = functools.reduce(lambda result, no : {'even' : result['even'] + 1, 'odd' : result['odd']} if no % 2 == 0 else { 'odd' : result['odd'] + 1, 'even' : result['even'] }, nos_list, {'even' : 0, 'odd' : 0})
 print(nos_stats)
