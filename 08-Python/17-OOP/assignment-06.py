b79041dae8dd70e6727b32d3e167bccdcf4787a0
Magesh Kuppan <tkmagesh77@gmail.com>
2023-08-17 02:19:03 +0530

    File persistance - assignment

---
+++ 08-Python/17-OOP/assignment-06.py
@@ -0,0 +1,15 @@
+""" 
+This is ONLY for ONE user account
+feel free to hardcode the userid, acctholder name etc
+IMPORTANT : Persist the transactions in a CSV file as and when they are performed so that when the application is executed again, all the transactions are loaded into the back account (from the CSV file)
+
+For CSV file operations, refer : https://docs.python.org/3/library/csv.html
+
+"""
+Menu:
+What operation would you like to do:
+    1. Deposit an amount into your account
+    2. Withdraw an amount from your account
+    3. Retrieve current balance
+    4. See previous transactions
+    5. Close Application
