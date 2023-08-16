8966b85b38646bc2e298afc656b308585d6d32ea
Magesh Kuppan <tkmagesh77@gmail.com>
2023-08-16 19:51:55 +0530

    more on classes

--- 08-Python/17-OOP/class-demo.py
+++ 08-Python/17-OOP/class-demo.py
@@ -20,13 +20,21 @@ class Employee:
     """ 
     
     #first_name & last_name can be passed by the user, and optional.
-    
+    # __init__ is invoked whenever a new instance of this class is created
     def __init__(self, fName="", lName=""): # self => reference of the newly created object
         print("A new employee instance is being created")
         self.first_name = fName
         self.last_name = lName 
+        self.days_worked = 0
     
-      
+    # __str__ is invoked when the object is converted to a string 
+    def __str__(self):
+        return "This is an employee"
 
-# instance created using the class like a function
-e = Employee()
+    # __repr__ is invoked to get the object string to be displayed
+    # "f{obj!r}"
+    def __repr__(self):
+        return f"firstName = {self.first_name}, lastName = {self.last_name}, daysWorked = {self.days_worked}"
+    
+    def mark_attendance(self):
+        self.days_worked += 1
