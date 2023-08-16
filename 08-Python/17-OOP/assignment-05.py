cc6c4743834025d3f2dee196c4c73a747ef37017
Magesh Kuppan <tkmagesh77@gmail.com>
2023-08-16 20:44:23 +0530

    assignment - updated

--- 08-Python/17-OOP/assignment-05.py
+++ 08-Python/17-OOP/assignment-05.py
@@ -10,3 +10,34 @@ Create a BankAccount class
             validation : money cannot be negative
         GetBalance()
 """
+
+class BankAccount:
+    def __init__(self, acctId, holderName):
+        self.AcctId = acctId
+        self.AcctHolderName = holderName
+        self.balance = 0
+    def deposit(self, amount):
+        if amount < 0 :
+            # raise an exception
+            pass
+        self.balance += amount    
+    def withdraw(self, amount):
+        if amount < 0 :
+            # raise an exception
+            pass
+        # ensure there is sufficient balance to perform withdrawl
+        """ 
+        if insufficentBalance:
+            # raise an exception
+            pass 
+        """
+        self.balance -= amount
+        
+    def getBalance(self):
+        #calculate the balance from the transactions performed and return the balance
+        pass
+    
+    def transactionHistory(self):
+        #return all the transactions
+        pass
+        
