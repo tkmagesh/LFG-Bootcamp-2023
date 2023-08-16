c158780b2508cfa85f28e63bf5169ee78c2ee61e
Magesh Kuppan <tkmagesh77@gmail.com>
2023-08-16 20:19:45 +0530

    Class - assignment

---
+++ 08-Python/17-OOP/assignment-05.py
@@ -0,0 +1,12 @@
+""" 
+Create a BankAccount class 
+    - state
+        AcctId
+        AcctHolderName
+    - behaviors
+        Deposit
+            validation : money cannot be negative
+        Withdraw
+            validation : money cannot be negative
+        GetBalance()
+"""
