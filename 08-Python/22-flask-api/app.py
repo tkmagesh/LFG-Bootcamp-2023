2f6dcbb6f3b14b46b9808d480c1bdf3d22073a9e
Magesh Kuppan <tkmagesh77@gmail.com>
2023-08-18 19:56:42 +0530

    handlers by request method

--- 08-Python/22-flask-api/app.py
+++ 08-Python/22-flask-api/app.py
@@ -1,10 +1,36 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 
 app = Flask(__name__)
-
-@app.route('/') # => When a request is make for "/", execute the function and return the result of the function to the user
+""" 
+@app.route('/', methods=['GET', 'POST']) # => When a request is make for "/", execute the function and return the result of the function to the user
 def index():
+    # returing a string response
     #return "Welcome to Flask!"
-    return render_template('index.html')
+    
+    # returning the contents of a file as response
+    #return render_template('index.html')
+    
+    
+    #return the data in JSON format
+    if request.method == 'GET':
+        data = {"req-method" : "GET", "name" : "Magesh"}
+        return jsonify(data)
+    else:
+        data = {"req-method" : "POST","name" : "Magesh"}
+        return jsonify(data) 
+    """
+
+
+@app.route('/', methods=['GET']) 
+def get_index():
+    #return the data in JSON format
+    data = {"req-method" : "GET", "name" : "Magesh"}
+    return jsonify(data)
+    
+@app.route('/', methods=['POST'])
+def post_index():
+    #return the data in JSON format
+    data = {"req-method" : "POST", "name" : "Magesh"}
+    return jsonify(data)
 
 app.run(port=8080, debug=True)
