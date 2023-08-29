ff56041502b35a41306b4c721d752dadc0685d9f
Magesh Kuppan <tkmagesh77@gmail.com>
2023-08-30 01:24:05 +0530

    updates

--- 08-Python/25-flask-restx-prod/server/app/api/__init__.py
+++ 08-Python/25-flask-restx-prod/server/app/api/__init__.py
@@ -1,7 +1,7 @@
 from flask_restx import Api
 
-from app.api.movies.views import movies_namespace
-from app.api.ping.views import ping_namespace
+from app.api.movies.handlers import movies_namespace
+from app.api.ping.handlers import ping_namespace
 
 
 api = Api(version="1.0", title="Flask API", doc="/api/v1/docs")
