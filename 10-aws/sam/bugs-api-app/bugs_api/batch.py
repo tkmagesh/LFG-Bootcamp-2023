fad796c869fb7ad1607ef5ad191fab852fe9f1ca
Magesh Kuppan <tkmagesh77@gmail.com>
2023-09-21 23:37:39 +0530

    publishing messages to SNS topic

--- 10-aws/sam/bugs-api-app/bugs_api/batch.py
+++ 10-aws/sam/bugs-api-app/bugs_api/batch.py
@@ -4,12 +4,14 @@ import os
 import logging
 
 s3 = boto3.client('s3')
+sns_client = boto3.client('sns')
 logger = logging.getLogger('bugsbatchprocessing')
 logger.setLevel(logging.INFO)
 
 
 # function to execute whenever a data file (json) is dropped in the S3 bucket
 def lambda_handler(event, context):
+    topic = os.environ.get('BUG_CREATE_TOPIC')
     bucket = event['Records'][0]['s3']['bucket']['name']
     file_name = event['Records'][0]['s3']['object']['key']
     logger.info('Bucket: ' + bucket)
@@ -20,5 +22,11 @@ def lambda_handler(event, context):
     file_content = obj['Body'].read().decode('utf-8')
     bugsToBeProcessed = json.loads(file_content)
     for bug in bugsToBeProcessed:
-        # process the bug
-        logger.info(bug)
+        # save the bug in the db
+        # ...logger.info(bug)
+        # send a message to the SNS topic
+        sns_client.publish(
+            TopicArn=topic,
+            Message=json.dumps({'default' : json.dumps(bug)}),
+            MessageStructure='json'
+        )
